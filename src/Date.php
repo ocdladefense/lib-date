0043bdc957aee81da8d18fa648f834419585ca25
Jose Bernal <jbernal.web.dev@gmail.com>
2022-03-05 13:01:25 -0800

    Migrate oft-used functions from a module.

--- src/Date.php
+++ src/Date.php
@@ -52,4 +52,38 @@ class Date {
 		
 		return sprintf($message, $text);
 	}
+
+
+
+
+	public function getStringMonth($numMonth){
+		
+		$numMonth = strlen($numMonth) == 1 ? "0$numMonth" : $numMonth;
+
+		return $this->getMonths()[$numMonth];
+	}
+
+
+
+
+	public function getMonths(){
+
+		return array(
+			"" 	   => "All Months",
+			"01"   => "January",
+			"02"   => "February",
+			"03"   => "March",
+			"04"   => "April",
+			"05"   => "May",
+			"06"   => "June",
+			"07"   => "July",
+			"08"   => "August",
+			"09"   => "September",
+			"10"   => "October",
+			"11"   => "November",
+			"12"   => "December"
+		);
+	}
+
+
 }
